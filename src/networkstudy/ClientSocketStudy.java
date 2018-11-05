4403ed6b4c65333133b1a382b61a1e81348f1521
YounSJ <kses3311@naver.com>
2018-11-05 15:21:33 +0900

    Update Chat project

--- src/networkstudy/ClientSocketStudy.java
+++ src/networkstudy/ClientSocketStudy.java
@@ -1,20 +1,57 @@
 package networkstudy;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientSocketStudy {
 	public static void main(String[] args) {
 		
 		Socket socket = null;
 		
+		OutputStream outputStream = null;
+		DataOutputStream dataOutputStream = null;
+		
+		InputStream inputStream = null;
+		DataInputStream dataInputStream = null;
+		
+		Scanner scanner = null;
+		
 		try {
 			socket = new Socket("localhost", 9000);
 			System.out.println("Server accept");
-			System.out.println("Socket: " + socket);
+			
+			outputStream = socket.getOutputStream();
+			dataOutputStream = new DataOutputStream(outputStream);
+			
+			inputStream = socket.getInputStream();
+			dataInputStream = new DataInputStream(inputStream);
+			
+			scanner = new Scanner(System.in);
+			
+			while(true) {
+				System.out.println("message ");
+				String outMessage = scanner.nextLine();
+				dataOutputStream.writeUTF(outMessage);
+				dataOutputStream.flush();
+				
+				String inMesssage = dataInputStream.readUTF();
+				System.out.println("inMessage: " + inMesssage);
+				
+				if(outMessage.equals("stop")) break;
+			}
+			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
 			try {
+				if(dataOutputStream != null) dataOutputStream.close();
+				if(outputStream != null) outputStream.close();
+				if(dataInputStream != null) dataInputStream.close();
+				if(inputStream != null) inputStream.close();
 				if(socket != null) socket.close();
 			}catch(Exception e) {
 				e.printStackTrace();
--- src/networkstudy/ServerSocketStudy.java
+++ src/networkstudy/ServerSocketStudy.java
@@ -1,5 +1,9 @@
 package networkstudy;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,6 +12,12 @@ public class ServerSocketStudy {
 		ServerSocket ss = null;
 		Socket socket = null;
 		
+		OutputStream outputStream = null;
+		DataOutputStream dataOutputStream = null;
+		
+		InputStream inputStream = null;
+		DataInputStream dataInputStream = null;
+		
 		try {
 			ss = new ServerSocket(9000);
 			System.out.println("Client ready");
@@ -15,11 +25,32 @@ public class ServerSocketStudy {
 			socket = ss.accept();
 			System.out.println("Client accept");
 			System.out.println("Socket: " + ss);
+			
+			outputStream = socket.getOutputStream();
+			dataOutputStream = new DataOutputStream(outputStream);
+			
+			inputStream = socket.getInputStream();
+			dataInputStream = new DataInputStream(inputStream);
+			
+			while(true) {
+				String clientMessage = dataInputStream.readUTF();
+				System.out.println("clientMessage: " + clientMessage);
+				
+				dataOutputStream.writeUTF("Message complete");
+				dataOutputStream.flush();
+				
+				if(clientMessage.equals("stop")) break;
+			}
 		
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
 			try {
+				if(dataInputStream != null) dataInputStream.close();
+				if(inputStream != null) inputStream.close();
+				if(dataOutputStream != null) dataOutputStream.close();
+				if(outputStream != null) outputStream.close();
+				
 				if(socket != null) socket.close();
 				if(ss != null) ss.close();
 			}catch(Exception e) {
